bca80e1ddd9253d52ef80e6f9eb08d8999b00c8d
Larzai <rifaldosaputra917@gmail.com>
2026-08-27 18:15:38 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -26,9 +26,9 @@ export default function Home() {
   const [circleHeight, setCircleHeight] = useState(30)
   const [blur, setBlur] = useState(false);
   const timeoutRef = useRef(null);
-  
+
   const images = [
-    "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?q=80&w=500&auto=format",
+    "https://images.unsplash.com/photo-1480074A568708-e7b720bb3f09?q=80&w=500&auto=format",
     "https://images.unsplash.com/photo-1449844908441-8829872d2607?q=80&w=500&auto=format",
     "https://images.unsplash.com/photo-1452626212852-811d58933cae?q=80&w=500&auto=format",
     "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?q=80&w=500&auto=format"
